7fe4b9332bf74ecd30b66efd6ebe097307229776
idealindiska-gif <idealindiska@gmail.com>
2025-12-18 11:23:39 +0500

    Fix product images by adding unoptimized: true and missing hostname to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'crm.ideallivs.com',
+                pathname: '/wp-content/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ideallivs.com',
                 pathname: '/wp-content/uploads/**',
             },
         ],
